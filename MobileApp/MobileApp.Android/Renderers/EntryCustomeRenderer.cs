d95b828c5b196a7930e4839f5aa8a60f6c079587
ocph23@gmail.com <ocph23@gmail.com>
2020-09-01 11:56:10 +0900

    First

---
+++ MobileApp/MobileApp.Android/Renderers/EntryCustomeRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MobileApp.Controls;
+using MobileApp.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(EntryCustom), typeof(EntryCustomeRenderer))]
+namespace MobileApp.Droid.Renderers
+{
+    class EntryCustomeRenderer : EntryRenderer
+    {
+        public EntryCustomeRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(global::Android.Graphics.Color.Transparent);
+
+
+            }
+        }
+    }
+}
---
+++ MobileApp/MobileApp/Controls/NeuButton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MobileApp.Controls
+{
+    public class NeuButton : Button
+    {
+        public NeuButton()
+        {
+        }
+    }
+}
